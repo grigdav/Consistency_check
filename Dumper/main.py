5761d7d5985b226e195c9211530f56dfd44dc9b7
Grigoryev David <grigdave@gmail.com>
2018-02-02 10:13:22 +0400

    Создание структуры модуля Dumper

---
+++ Dumper/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Главный модуль, точка запуска.
+
+
+
+#getter = newGetter ()        # Создание вызовов модулей
+#parser = newParser()
+#storer = newStorer()
+
+
+#XML = getter.get()           # Cхема передачи данных - в виде цепочки.
+#DS = parser.parse(XML)
+#R = storer.store(DS)
+
+
+#if __name__ == '__main__':   # Структура запсука модуля
+#	main()
